ab65d8520706d248ff0f53c10a627b47c73d1aa9
Pablo Maldonado <p.maldonado@colostate.edu>
2022-02-21 16:47:05 -0700

    adding to package

--- R/flowmalizr.R
+++ R/flowmalizr.R
@@ -1,6 +1,4 @@
-library(tidyverse)
-
-#' flowmalizr
+#' make_df
 #'
 #' @param file
 #'
@@ -8,14 +6,30 @@ library(tidyverse)
 #' @export
 #'
 #' @examples
+#                      work on exp groups vs control groups
+make_df <- function(path){
+   xlsx_file <- readxl::read_excel(path)
+   df <- tidyr::pivot_longer(xlsx_file, cols = -c(1:3))
 
-df <- readxl::read_excel("example_data/example_data.xlsx")
-df <- tidyr::pivot_longer(df, cols = -c(1:3))
+return(df)
+}
+
+make_df("example_data/example_data.xlsx")
 
 
+
+#' flowmalizr
+#'
+#' @param df
+#'
+#' @return
+#' @importFrom magrittr %>%
+#' @export
+#'
+#' @examples
 flowmalizr <- function(df){
    df <- df %>%
-     mutate(cells_from_total = total_cell_count_per_mL*value/live_cells) %>%
+     dplyr::mutate(cells_from_total = total_cell_count_per_mL*value/live_cells) %>%
      select(1, 2, 4, 6) %>%
        mutate(percentage_of_total = cells_from_total/total_cell_count_per_mL*100) %>%
        arrange(percentage_of_total)
@@ -25,3 +39,4 @@ return(df)
 
 flowmalizr(df)
 
+#                             group vs cell pops
