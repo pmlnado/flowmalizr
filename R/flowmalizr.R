1755ce20d3c52f9acc53b3ff6ed6b7ecdd74976b
Pablo Maldonado <p.maldonado@colostate.edu>
2022-02-21 00:04:07 -0700

    starting git repository

---
+++ R/flowmalizr.R
@@ -0,0 +1,24 @@
+
+#' flowmalizr
+#'
+#' @param file
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+df <- readxl::read_excel("example_data/example_data.xlsx")
+df <- tidyr::pivot_longer(df, cols = -c(1:3))
+
+flowmalizr <- function(df){
+    library(tidyverse)
+   df <- df %>%
+     mutate(cells = (df[,"total_cell_count_per_mL"]*df[,"value"])/(df[,"live_cells"])) %>%
+     arrange(cells) %>%
+     select(1, 4, 6)
+
+return(df)
+}
+
+flowmalizr(df)
---
+++ R/scratch.R
@@ -0,0 +1,7 @@
+library(devtools)
+library(roxygen2)
+library(testthat)
+library(available)
+available("flowmalizr")
+
+#create_package("/Users/PabloM/Documents/R_packages/flowmalizr")
