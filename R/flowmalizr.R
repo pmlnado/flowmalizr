2961b22a04aae3117ad2c9fe987f62099010aa3c
Pablo Maldonado <p.maldonado@colostate.edu>
2022-02-28 15:28:20 -0700

    combined code chunks

--- R/flowmalizr.R
+++ R/flowmalizr.R
@@ -3,40 +3,23 @@
 #' @param file
 #'
 #' @return
-#' @export
-#'
-#' @examples
-#                      work on exp groups vs control groups
-make_df <- function(path){
-   xlsx_file <- readxl::read_excel(path)
-   df <- tidyr::pivot_longer(xlsx_file, cols = -c(1:3))
-
-return(df)
-}
-
-make_df("example_data/example_data.xlsx")
-
-
-
-#' flowmalizr
-#'
-#' @param df
-#'
-#' @return
 #' @importFrom magrittr %>%
 #' @export
 #'
 #' @examples
-flowmalizr <- function(df){
+#                      work on exp groups vs control groups
+flowmalizr <- function(path){
+   xlsx_file <- readxl::read_excel(path)
+   df <- tidyr::pivot_longer(xlsx_file, cols = -c(1:3))
    df <- df %>%
-     dplyr::mutate(cells_from_total = total_cell_count_per_mL*value/live_cells) %>%
-     select(1, 2, 4, 6) %>%
-       mutate(percentage_of_total = cells_from_total/total_cell_count_per_mL*100) %>%
-       arrange(percentage_of_total)
+      dplyr::mutate(cells_from_total = total_cell_count_per_mL*value/live_cells) %>%
+      dplyr::select(1, 2, 4, 6) %>%
+      dplyr::mutate(percentage_of_total = cells_from_total/total_cell_count_per_mL*100)
 
 return(df)
 }
 
-flowmalizr(df)
+flowmalizr("example_data/example_data.xlsx")
+
 
 #                             group vs cell pops
