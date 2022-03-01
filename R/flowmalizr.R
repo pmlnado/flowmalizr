e10f9a8cb42d372001f93c4c4bdee00c64b36b91
Pablo Maldonado <p.maldonado@colostate.edu>
2022-02-28 17:39:23 -0700

    adding functions

--- R/flowmalizr.R
+++ R/flowmalizr.R
@@ -9,14 +9,35 @@
 #' @examples
 #' path_to_data <- system.file("extdata", "example_data.xlsx", package = "flowmalizr")
 #' flowmalizr(path = path_to_data)
+
+# First function to normalize cell data
 flowmalizr <- function(path){
    xlsx_file <- readxl::read_excel(path)
    df <- tidyr::pivot_longer(xlsx_file, cols = -c(1:3))
    df <- df %>%
       dplyr::mutate(cells_from_total = total_cell_count_per_mL*value/live_cells) %>%
       dplyr::select(1, 2, 4, 6) %>%
-      dplyr::mutate(percentage_of_total = cells_from_total/total_cell_count_per_mL*100)
+      dplyr::mutate(percentage_of_total = cells_from_total/total_cell_count_per_mL*100) %>%
+      dplyr::arrange(percentage_of_total)
 
-return(df)
+   return(df)
+df <- return(df)
 }
+
+# Pull unique gated populations
+unique.pops <- function(df){
+   unique <- df %>% dplyr::pull(name) %>%
+   unique()
+
+return(unique)
+}
+
+# Plot percentage of unique populations
+function(df){
+pop <- ggplot2::ggplot(df) +
+   ggplot2::geom_bar(ggplot2::aes(y = df$percentage_of_total, fill = name))
+
+return(pop)
+}
+
 
