2e0204fc48695f6431c93a916db1c90158439f43
Pablo Maldonado <p.maldonado@colostate.edu>
2022-03-20 14:04:55 -0600

    formatting

--- scratch/scratch.R
+++ scratch/scratch.R
@@ -18,7 +18,7 @@ view <- df %>% dplyr::group_by(name) %>%
   dplyr::summarise_at(vars(percentage_of_total),
                       funs(mean(.,na.rm=TRUE)))
 view <- view %>%
-  mutate(Perc = paste0(round(percentage_of_total, digits = 2), "%      "))
+  mutate(Perc = paste0(round(percentage_of_total, digits = 2), "%"))
 
 ggplot(view, aes(x = fct_reorder(name, percentage_of_total),
                  y = percentage_of_total, fill = name)) +
@@ -26,7 +26,8 @@ ggplot(view, aes(x = fct_reorder(name, percentage_of_total),
   ylim(0, 100) +
   coord_flip() +
   labs(x = "Phenotype", y = "Mean Percent") +
-  geom_text(aes(label=Perc), position=position_dodge(width = 0), hjust = -.1, size = 3)
+  geom_text(aes(label=Perc), position=position_dodge(width = 0), hjust = -.1,
+            size = 3)
 
 
 
